cd5879b535bf4d32773fef81686935007a4972b4
Diya <diyamaru32@gmail.com>
2026-06-13 18:03:06 +0530

    Add files via upload

---
+++ Flutter/Lab22/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:lab22/p1.dart';
+
+void main() {
+  runApp(const Main());
+}
+
+class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return (MaterialApp(home: Scaffold(body: p1())));
+  }
+}
